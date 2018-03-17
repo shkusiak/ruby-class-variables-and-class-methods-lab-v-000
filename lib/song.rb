2543f88dc784381afec5b1f261efe08bc78b492f
Shea Kusiak <shkusiak@gmail.com>
2018-03-17 19:39:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,11 @@ class Song
   def self.genre_count
     hash = {}
     @@genres.each do |genre|
-      hash[genre] = 1
+      if hash.include?(genre)
+        hash[genre] += 1
+      else
+        hash[genre] = 1
+      end
     end
     hash
   end
